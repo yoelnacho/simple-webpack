c75b0e64ccd1895881728fd272419c49c5236eee
Yoelnacho <yoelnacho@gmail.com>
2019-03-24 12:27:19 -0300

    file plugin was added: png, jpg, svg and gif files

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,12 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   },
